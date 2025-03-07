ce38fdb383fad5bffffc88baee59e45e545e0ca3
Michaelliu1017 <liuguoliang1017@gmail.com>
2025-03-07 15:13:59 -0500

    Add 2 files

---
+++ cfile.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("Output of C Program");
+}
---
+++ pyfile.py
@@ -0,0 +1,5 @@
+def main():
+    print("Output of Python Program")
+
+if __name__ == "__main__":
+    main()
