83bea0f383cb042bc539c4eb9eaf75340e86cf73
Michaelliu1017 <liuguoliang1017@gmail.com>
2025-03-11 14:32:47 -0400

    rewrite py file

--- pyfile.py
+++ pyfile.py
@@ -1,5 +1,62 @@
+import os
+import time
+import datetime
+import platform
+
+
+def gl_banner_display():
+    print("\n")
+    seed = "490000514095612094711092719200618300609300923300527300409400454500"
+    length = len(seed)
+
+    for i in range(11):
+        
+        start_index = length - 6 * (i + 1)
+        end_index = length - 6 * i
+        seed_cup = seed[start_index:end_index]
+
+        sub_seed = int(seed_cup)
+      
+        for _ in range(3):
+            emp = sub_seed % 10   
+            sub_seed //= 10       
+
+            print(" " * emp, end="")  
+
+            star = sub_seed % 10    
+            sub_seed //= 10
+
+            print("*" * star, end="") 
+
+        time.sleep(0.05)
+        print()  
+    
+    projectName="SWE Project 3"
+    db_name="None"
+    environment="Testing"
+    version="1.0"
+    extra_info="Web Crawlers"
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    os_info = platform.platform()
+
+    banner = f"""
+
+-- DATABASE PROGRAM INITIALIZATION --
+
+   PROJECT      : {projectName}
+   DB NAME      : {db_name}
+   ENVIRONMENT  : {environment}
+   VERSION      : {version}
+   DATE & TIME  : {current_time}
+   OS           : {os_info}
+   INFO         : {extra_info}
+
+"""
+    print(banner.strip())
+
 def main():
-    print("Output of Python Program")
+    #print("Output of Python Program")
+    testInfo()
 
 if __name__ == "__main__":
     main()
